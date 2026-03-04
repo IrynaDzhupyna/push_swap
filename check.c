55337d716a7e5a29e7560b63e00527dbde213d3b
Iryna Dzhupyna <irdzhupy@c4b11c6.42berlin.de>
2026-03-04 12:06:29 +0100

    new input check to check

--- check.c
+++ check.c
@@ -6,7 +6,7 @@
 /*   By: irdzhupy <irdzhupy@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 17:40:19 by irdzhupy          #+#    #+#             */
-/*   Updated: 2026/03/03 18:24:43 by irdzhupy         ###   ########.fr       */
+/*   Updated: 2026/03/04 12:06:09 by irdzhupy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,43 +21,35 @@ int	parse_int_check(const char *s, int *int_out)
 
 	i = 0;
 	sign = 1;
+	num = 0;
 	if (!s || !*s)
 		return (0);
 	while (s[i])
 	{
 		if ((9 <= s[i] && s[i] <= 13) || s[i] == 32)
 			i++;
-		else if (s[i] == '+' || s[i] == '-')
+		else if ((s[i] == '+' || s[i] == '-') && ('0' <= s[i + 1] && s[i + 1] <= '9'))
 		{
-			if ('0' <= s[i + 1] && s[i + 1] <= '9')
-			{
 				if (s[i] == '-')
 					sign = -1;
+				i++;
+		}
+
+		if (sign == -1)
+			limits = -2147483648L;
+		else
+			limits = 2147483647L;
+
+		while ('0' <= s[i] && s[i] <= '9' || s[i] != '\0')
+		{
+			if ((num * 10 + (s[i] - 48)) < limits)
+			{
+				num = num * 10 + (s[i] - 48);
 				i++;
 			}
 			else
 				return (0);
 		}
-		//before reading digits we need to check limits
-		//L means this num is long not int, 
-		if (sign == -1)
-			limits = -2147483648L;
-		else
-			limits = 2147483647L;
-		else if ('0' <= s[i] && s[i] <= '9')
-		{
-			num = 0;
-			while ('0' <= s[i] && s[i] <= '9')
-			{
-				if (INT_MIN < (num * 10 + (s[i] - '0')) && (num * 10 + (s[i] - '0')) < INT_MAX)
-					num = num * 10 + (s[i] - '0');
-				else
-					return (0);
-				i++;
-			}
-		}
-		else
-			return (0);
 	}
 	if (s[i] != '\0')
 		return (0);
