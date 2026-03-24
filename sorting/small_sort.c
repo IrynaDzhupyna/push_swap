6c8fbedd68a2ecad81af99fb9f214b8e5324b474
Iryna Dzhupyna <irdzhupy@c3a8c1.42berlin.de>
2026-03-24 14:50:54 +0100

    small_sort.c cleaned

--- sorting/small_sort.c
+++ sorting/small_sort.c
@@ -6,12 +6,29 @@
 /*   By: irdzhupy <irdzhupy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 12:03:23 by irdzhupy          #+#    #+#             */
-/*   Updated: 2026/03/24 14:09:06 by irdzhupy         ###   ########.fr       */
+/*   Updated: 2026/03/24 14:46:32 by irdzhupy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "push_swap.h"
+
+void    find_push_smallest(s_node **stack_head_a, s_node **stack_head_b)
+{
+    s_node  *min_node;
+    int     index_smallest;
+
+    min_node = smallest_node(*stack_head_a);
+    index_smallest = get_node_position(*stack_head_a, min_node);
+    while (*stack_head_a != min_node)
+    {
+        if (index_smallest <= 2)
+            rotate(stack_head_a);
+        else
+            reverse_rotate(stack_head_a);
+    }
+    push(stack_head_b, stack_head_a);
+}
 
 void	sort_three(s_node **stack_head)
 {
@@ -44,70 +61,23 @@ void	sort_three(s_node **stack_head)
 		reverse_rotate(stack_head);
 }
 
-//add index to smallest_node()
-//if index <= 2 ra if index > 2 rra
 void    sort_four(s_node **stack_head_a, s_node **stack_head_b)
-{
-    s_node  *min_node;
-    int index_smallest;
-
-    min_node = smallest_node(*stack_head_a);
-    index_smallest = get_node_position(*stack_head_a, min_node);
-    printf("SMALLEST FIRST INDEX is %d\n", index_smallest);
-    while (*stack_head_a != min_node)
-    {
-        if (index_smallest <= 2)
-        {
-            rotate(stack_head_a);
-            printf("\n+++\n");
-        }
-        else
-        {
-            reverse_rotate(stack_head_a);
-            printf("\n---n");
-        }
-    }
-    push(stack_head_b, stack_head_a);
+{   
+    find_push_smallest(stack_head_a, stack_head_b);
     sort_three(stack_head_a);
     push(stack_head_a, stack_head_b);
 }
 
 void	sort_five(s_node **stack_head_a, s_node **stack_head_b)
-{
-	s_node	*min_node;
-	int		index_smallest;
-
-	min_node = smallest_node(*stack_head_a);
-    index_smallest = get_node_position(*stack_head_a, min_node);
-    printf("SMALLEST FIRST INDEX is %d\n", index_smallest);
-    //the same as sort_four
-	while (*stack_head_a != min_node)
-	{
-		if (index_smallest <= 2)
-			rotate(stack_head_a);
-		else
-			reverse_rotate(stack_head_a);
-        printf("\n+\n");
-	}
-	push(stack_head_b, stack_head_a);
-    
-	min_node = smallest_node(*stack_head_a);
-    index_smallest = get_node_position(*stack_head_a, min_node);
-    printf("SMALLEST FIRST INDEX is %d\n", index_smallest);
-	while (*stack_head_a != min_node)
-	{
-		if (index_smallest <= 1)
-			rotate(stack_head_a);
-		else
-			reverse_rotate(stack_head_a);
-	}
-	push(stack_head_b, stack_head_a);
+{   
+    find_push_smallest(stack_head_a, stack_head_b);
+    find_push_smallest(stack_head_a, stack_head_b);
 	sort_three(stack_head_a);
 	push(stack_head_a, stack_head_b);
 	push(stack_head_a, stack_head_b);
 }
 
-void    small_sort(s_node **stack_head_a, s_node **stack_head_b, int size)
+/*void    small_sort(s_node **stack_head_a, s_node **stack_head_b, int size)
 {
     if (size < 2)
         return ;
@@ -131,4 +101,18 @@ void    small_sort(s_node **stack_head_a, s_node **stack_head_b, int size)
         if (!stack_is_sorted(*stack_head_a))
             sort_five(stack_head_a, stack_head_b);
     }
+}*/
+
+void    small_sort(s_node **stack_head_a, s_node **stack_head_b, int size)
+{
+        if (size < 2)
+        return ;
+    else if (size == 2)
+            swap(stack_head_a);   
+    else if (size == 3)
+            sort_three(stack_head_a);
+    else if (size == 4)
+            sort_four(stack_head_a, stack_head_b);
+    else if (size == 5)
+            sort_five(stack_head_a, stack_head_b);
 }
